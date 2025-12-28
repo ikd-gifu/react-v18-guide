b0709b9724f28caa935b86e63998be89eb6e9214
ikd-gifu <oki19ki@gmail.com>
2025-12-28 19:14:57 +0900

    ss3 20まで完了

--- react-guide-material/03_js_basic/080_truthy_falsy/start/main.js
+++ react-guide-material/03_js_basic/080_truthy_falsy/start/main.js
@@ -11,7 +11,7 @@ undefined
 NaN (Not a Number)
 */
 
-const a = 0;
+const a = "0";
 let result = a ? 10 : -10;
 console.log(result);
 
@@ -25,15 +25,21 @@ const resultA = "" && "foo";
 const resultB = 2 && 1 && 0 && 3;
 const resultC = "foo" && 4;
 
-// console.log(resultA);
-// console.log(resultB);
-// console.log(resultC);
+// 左から順に評価していき、「最初に見つかった falsy の値」を返す。
+// もし最後まで falsy が見つからなければ、「最後の値」を返す。
+// 単に true / false を返すわけではない。
+console.log(resultA); // ""
+console.log(resultB); // 0
+console.log(resultC); // 4
 
 // 理論和 (||) について
 const resultD = "" || "foo";
 const resultE = 0 || 2 || 0;
 const resultF = "foo" || 4;
 
-console.log(resultD);
-console.log(resultE);
-console.log(resultF);
+// A || B は && と逆のイメージで動きます。
+// 左から順に評価していき、「最初に見つかった truthy の値」を返す。
+// もし最後まで truthy が見つからなければ、「最後の値」を返す。
+console.log(resultD); // "foo"
+console.log(resultE); // 2
+console.log(resultF); // "foo"
