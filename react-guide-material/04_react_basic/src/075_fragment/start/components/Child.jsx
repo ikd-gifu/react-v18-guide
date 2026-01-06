abe49d220a13a67caf1ae30de5aacb23790de70d
ikd-gifu <oki19ki@gmail.com>
2026-01-06 11:59:07 +0900

    ss4 31

--- react-guide-material/04_react_basic/src/075_fragment/start/components/Child.jsx
+++ react-guide-material/04_react_basic/src/075_fragment/start/components/Child.jsx
@@ -1,10 +1,16 @@
 import "./Child.css";
+// import React from "react"; // Reactフラグメントを使うためにReactをインポートする必要がある
+// import { Fragment } from "react"; // Reactフラグメントを使うためにFragmentをインポートする。key属性も指定できる
 
 const Child = () => {
-  return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
+  return ( // returnで返すJSXは1つの要素である必要がある
+    <>
+      <div className="component">
+        <h3>Hello Component</h3>
+      </div>
+      <h3>Hello Fragment </h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos aperiam atque itaque reprehenderit officia blanditiis rem consequatur repellendus, aliquid exercitationem tempora cum. Eaque, aliquid reiciendis rem nesciunt vitae accusantium debitis.</p>
+    </>
   );
 };
 
