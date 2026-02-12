6038d436f6b987af5f7737b2b0c9a983df6bc369
ikd-gifu <oki19ki@gmail.com>
2026-02-12 10:34:35 +0900

    ss16 040 176データ表示

--- react-guide-material/16_rest_api/src/040_axios_get_request/start/Example.jsx
+++ react-guide-material/16_rest_api/src/040_axios_get_request/start/Example.jsx
@@ -1,6 +1,7 @@
 // axiosでリクエストを送信
 import { useEffect } from "react";
 import axios from "axios"; // 内部でJSONを内部でJSオブジェクトに変換してくれる
+// https://axios-http.com/ja/
 
 const Example = () => {
   // コンポーネント内は純関数にする 副作用はuseEffect内で行う
@@ -9,10 +10,10 @@ const Example = () => {
   useEffect(() => { // useEffect内で副作用を扱う コールバック自体は同期的に実行される
     const getUser = async () => { // 関数を定義しただけで実行されない async awaitで非同期処理を同期的に扱う
       const res = await axios.get('http://localhost:3003/user')
-      console.log(res.data)
+      console.log(res.data) // 状態更新をしないので、再レンダリングは発生しない
     };
     getUser(); // 非同期処理は内側の関数に切り出してから明示的に呼び出す
-  });
+  }); // []はなくても無限ループにはならない。親の再レンダリングや自分の再レンダリングが起きた場合に再度実行される
 };
 
 export default Example;
--- react-guide-material/16_rest_api/src/050_axios_get_state/start/Example.jsx
+++ react-guide-material/16_rest_api/src/050_axios_get_state/start/Example.jsx
@@ -1,5 +1,32 @@
+import axios from 'axios';
+import { useEffect, useState } from "react";
+
 const Example = () => {
-  return <></>;
+  console.log('Exampleレンダリング');
+  // mapを呼ぶため、初期値を配列にするか、optional chainingを使う
+  const [ users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUser = async () => {
+      const res = await axios.get('http://localhost:3003/user')
+      setUsers(res.data); // 状態更新をすると際レンダリングされ、useEffectが再実行される
+    }
+    getUser();
+  }, []); // POINT 空の配列を渡すことで、最初の1回だけ実行される
+
+  return (
+    <div>
+      {users.map( user => { // users?.mapを使う場合もある
+        return (
+          <div key={user.id}>
+            <h3>{user.username}</h3>
+            <p>{user.age}歳</p>
+            <p>{user.hobbies.join(',')}</p>
+          </div>
+        )
+      })}
+    </div>
+  );
 };
 
 export default Example;
