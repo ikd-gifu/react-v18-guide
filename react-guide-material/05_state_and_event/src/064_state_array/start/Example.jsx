e705ee6598887872eafc5787e5ae1b3d49a46670
ikd-gifu <oki19ki@gmail.com>
2026-02-19 11:13:40 +0900

    ss5 54, 55 array state

--- react-guide-material/05_state_and_event/src/064_state_array/start/Example.jsx
+++ react-guide-material/05_state_and_event/src/064_state_array/start/Example.jsx
@@ -1,9 +1,30 @@
+import { useState } from 'react';
+
 const Example = () => {
   const numArray = [1, 2, 3, 4, 5];
-
+  const [nums, setNums] = useState(numArray);
+  
+  const shuffle = () => {
+    // numsを展開し、新しい配列を定義している。
+    const newNums = [ ...nums ];
+    // numsの持つ参照をnewNumsに代入する意味になる
+    // 配列自体がコピーされるわけではない
+    // const newNums = nums;
+    // 配列から最後の要素を取り除き、その要素を返す
+    const value = newNums.pop(); // newNums: [1, 2, 3, 4] value: 5
+    // 指定された要素を配列の先頭に追加し、新しい配列の長さを返す
+    newNums.unshift(value); // newNums:[5, 1, 2, 3, 4]
+    // newNums はローカル変数なので、setNums(newNums) を呼ぶまで 
+    // React の再レンダーが起きない
+    // 再レンダーで更新後のstateを用いて画面が更新される
+    setNums(newNums);
+    console.log(newNums);
+  }
   return (
+    // {}の中に配列を入れると展開される
     <>
-      
+      <h1>{nums}</h1>
+      <button onClick={shuffle}>shuffle</button>
     </>
   );
 };
--- react-guide-material/05_state_and_event/src/068_practice_obj_state/start/Example.jsx
+++ react-guide-material/05_state_and_event/src/068_practice_obj_state/start/Example.jsx
@@ -3,17 +3,50 @@ import { useState } from 'react';
 const Example = () => {
   const orderObj = { item: 'apple', count: 10 };
   const [order, setOrder] = useState(orderObj);
-  const changeItem = (e) => {};
-  const countUp = () => {};
-  const countDown = () => {};
+  // const changeItem = (e) => {
+  // order が指す同じオブジェクト本体を破壊的変更している
+  // order の中身（同一参照先）を直接更新している状態
+  //   order.item = e.target.value
+
+  // 新しいオブジェクトを作っているが、order.item = ... でオブジェクトを
+  // 直接壊しているので、「直接変更していない」とは言えない
+  //   setOrder({ ...order, item: e.target.value })
+  // };
+  // const countUp = () => {
+  //   count = order.count
+  //   setOrder({ ...order, count: count + 1 })
+  // };
+  // const countDown = () => {
+  //   count = order.count
+  //   setOrder({ ...order, count: count - 1 })
+  // };
+
+  const changeItem = (e) => {
+    // setOrder({ ...order, item: e.target.value });
+    // 元の値を使う場合はアロー関数を使う方がいい
+    // 新しい値がコールバック関数の戻り値になる
+    // オブジェクトを返す場合、それを示すために()をつける
+    // オブジェクトの{}かアロー関数の{}か区別するため
+    setOrder(order => ({...order, item: e.target.value }));
+    // エラー：Failed to reload /src/068_practice_obj_state/start/Example.jsx. 
+    // This could be due to syntax errors or importing non-existent modules. (see errors above)
+  };
+  const countUp = () => {
+    // setOrder({ ...order, count: order.count + 1 })
+    setOrder(order => ({ ...order, count: order.count + 1 }));
+  };
+  const countDown = () => {
+    // setOrder({ ...order, count: order.count - 1 })
+    setOrder(order => ({ ...order, count: order.count - 1 }));
+  };
   return (
     <div>
       <h3>練習問題</h3>
       <p>
         記述を変更し、完成コードのように「+と-ボタンをクリックするとCountの表示が1ずつ増減する機能」と「input要素に連動してItemの表示が変更される機能」を実装してください。コンポーネントの外側（上部）に変数や関数を準備しているためうまく使ってください。
       </p>
-      <h3>Item:{/* ここにorder.itemを表示してください。*/}</h3>
-      <h3>Count:{/* ここにorder.countを表示してください。*/}</h3>
+      <h3>Item:{order.item}</h3>
+      <h3>Count:{order.count}</h3>
       <input type="text" value={order.item} onChange={changeItem} />
       <button onClick={countUp}>+</button>
       <button onClick={countDown}>-</button>
