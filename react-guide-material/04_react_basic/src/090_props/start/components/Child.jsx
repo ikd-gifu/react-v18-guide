42b68350b6c21e01a2fd7fd4e8696d5c342e2a35
ikd-gifu <oki19ki@gmail.com>
2026-02-20 09:48:20 +0900

    ss4 35 props component

--- react-guide-material/04_react_basic/src/090_props/start/components/Child.jsx
+++ react-guide-material/04_react_basic/src/090_props/start/components/Child.jsx
@@ -5,9 +5,16 @@
 
 import "./Child.css";
 
-const Child = () => {
+// コンポーネントの出力結果を変更する際は、propsを用いて値を変更するルールがある
+// const Child = (props) => { // propsは慣習的な名称。pなどでも良い JSの機能
+const Child = ({ color: c = 'green' }) => {
+  // 分割代入 これもJSの機能
+  // デフォルト値を設定できる。別名を設定できる
+
+  // console.log(props);
+
   return (
-    <div className="component">
+    <div className={`component ${c}`}>
       <h3>Hello Component</h3>
     </div>
   );
