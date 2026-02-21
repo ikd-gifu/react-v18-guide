480089927873c711c9721fbb4480c42c4345c83d
ikd-gifu <oki19ki@gmail.com>
2026-02-21 11:26:00 +0900

    ss4 38 children

--- react-guide-material/04_react_basic/src/110_props_children/start/Example.jsx
+++ react-guide-material/04_react_basic/src/110_props_children/start/Example.jsx
@@ -2,16 +2,43 @@ import Profile from "./components/Profile";
 import Container from "./components/Container";
 
 const profile = [
-  { name: "Takashi", age: 19, country: "Japan", color: "green" },
-  { name: "Jane", age: 28, country: "UK", color: "blue" },
+  { name: "Takashi", age: 19, country: "Japan" },
+  { name: "Jane", age: 28, country: "UK", color: "red" },
 ];
 
+/* POINT コンポーネントの子要素は props.children に渡る！
+コンポーネントが子要素を持つ場合、props.children という特別なプロパティとして受け渡されます。
+ */
 const Example = () => {
   return (
     <div>
-      <Container title="Childrenとは？" />
+      {/* この中がchildren という特殊なpropsのプロパティとしてコンポーネントに渡される */}
+      {/* childrenを利用すると、コンポーネントを他のコンポーネントに渡すことができる */}
+      {/* <Container title="Childrenとは？" >
+        <Profile {...profile[0]} />
+        <Profile {...profile[1]} />
+      </Container> */}
+      <Container title="Childrenとは？"
+        first={<Profile key={profile[0].name} {...profile[0]} />}
+        second={<Profile key={profile[1].name} {...profile[1]} />}
+      />
     </div>
   );
 };
 
 export default Example;
+
+// 通常のプロパティの渡し方
+{/* <Container title="Childrenとは？" /> */}
+
+// propsに指定。非推奨
+{/* <Container title="Childrenとは？" children={
+  [
+    <Profile key={profile[0].name} {...profile[0]} />,
+    <Profile key={profile[1].name} {...profile[1]} />
+  ]
+}
+first={<Profile key={profile[0].name} {...profile[0]} />}
+second={<Profile key={profile[1].name} {...profile[1]} />}
+/>
+</div> */}
--- react-guide-material/04_react_basic/src/110_props_children/start/components/Container.jsx
+++ react-guide-material/04_react_basic/src/110_props_children/start/components/Container.jsx
@@ -1,9 +1,15 @@
 import "./Container.css";
 
-const Container = ({ title }) => {
+/* POINT props.children として子要素を受け取る
+ここでは分割代入を使用して、 props.children を取り出しています。
+*/
+const Container = ({ title, children, first, second }) => {
+  console.log(children);
   return (
     <div className="container">
       <h3>{title}</h3>
+      {second}
+      {first}
     </div>
   );
 };
