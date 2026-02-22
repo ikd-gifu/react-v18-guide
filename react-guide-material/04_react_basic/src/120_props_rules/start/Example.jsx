b1772525b9397b014da7e13f55f4e51a4d992756
ikd-gifu <oki19ki@gmail.com>
2026-02-22 09:54:18 +0900

    ss4 39 props rules

--- react-guide-material/04_react_basic/src/120_props_rules/start/Example.jsx
+++ react-guide-material/04_react_basic/src/120_props_rules/start/Example.jsx
@@ -1,14 +1,18 @@
 // POINT propsの流れは一方通行
+// 値を渡す場合は親コンポーネントからpropsを利用して渡す
+// Reactでは、子から渡す方法はない（
 // POINT propsは読み取り専用
 
 import Bye from "./components/Bye"
 import Hello from "./components/Hello"
 
 const Example = () => {
+  const name = 'Tom'; // POINT propsの流れは一方通行
+  // props.name = 'Tom'; // 定義前で使えない。代入しているのでNG
   return (
     <>
-      <Hello />
-      <Bye />
+      <Hello name={name}/>
+      <Bye name={name} />
     </>
   );
 };
--- react-guide-material/04_react_basic/src/120_props_rules/start/components/Hello.jsx
+++ react-guide-material/04_react_basic/src/120_props_rules/start/components/Hello.jsx
@@ -1,4 +1,12 @@
 const Hello = (props) => {
+  // const name = 'Tom'; // Helloでは使えるが、Byeには渡せない
+  // props.name = 'Bob'; // POINT propsは読み取り専用
+  console.log(props.name);
+  // オブジェクトの設定を可視化 (オブジェクト, 設定状態を見たいプロパティ)
+  // const desc = Reflect.getOwnPropertyDescriptor(props, 'name');
+  // console.log(desc);
+  // configurable: false // この設定は変更不可
+  // writable: false // 書き換え不可
   return (
     <div>
       <h3>Hello {props.name}</h3>
