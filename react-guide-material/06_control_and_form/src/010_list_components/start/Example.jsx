792372a967a64c4deb5989ea862fe2731a5b9613
ikd-gifu <oki19ki@gmail.com>
2026-02-24 09:35:49 +0900

    ss6 010 61 array list

--- react-guide-material/06_control_and_form/src/010_list_components/start/Example.jsx
+++ react-guide-material/06_control_and_form/src/010_list_components/start/Example.jsx
@@ -3,11 +3,26 @@ const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
 
+  const animalList = [];
+  for(const animal of animals) { // 文
+    animalList.push(<li>{animal}</li>)
+    // JSXはReact要素に変換される。オブジェクトとして扱えるので、配列に含めることができる
+  }
+
+  const helloAnimals = animals.map((animal) => // 式 新しい配列を返す Reactが展開して表示
+    <li >Hello, {animal}</li>
+    // <li key={`hello-{animal}`}>Hello, {animal}</li> // Missing "key" prop for element in iteratorの対処法
+  );
   return (
     <>
       <h3>配列の操作</h3>
       <ul>
-        
+        {/* <li>{animals[0]}</li>
+        <li>{animals[1]}</li>
+        <li>{animals[2]}</li> */}
+        {animalList}
+        {helloAnimals}
+        {animals.map((animal) => <li >Hello, {animal}</li>)}
       </ul>
     </>
   );
