089e3815d03ea97bf27a11bf016701ae525d4da9
ikd-gifu <oki19ki@gmail.com>
2026-02-26 08:54:37 +0900

    ss6 030 63 key test

--- react-guide-material/06_control_and_form/src/030_practice_list/start/Example.jsx
+++ react-guide-material/06_control_and_form/src/030_practice_list/start/Example.jsx
@@ -1,13 +1,32 @@
+import Profile from './components/Profile';
+
+const persons = [
+  {
+    name: 'Geo',
+    age: 20,
+    hobbies: ['sports', 'music']
+  },
+  {
+    name: 'Tom',
+    age: 20,
+    hobbies: ['movie', 'music']
+  },
+  {
+    name: 'Lisa',
+    age: 20,
+    hobbies: ['sports', 'travel', 'game']
+  }
+]
+
 const Example = () => {
   return (
-    <>
-      <h3>練習問題</h3>
-      <p>Profileコンポーネントを使用して、完成コードと同じ画面を作成してください。</p>
-      <p>また、Profileコンポーネント内のリスト表示部分にkeyを設定して、ワーニング表示がされないようにしてください。</p>
-      <ul>
-        {/* ここに記述 */}
-      </ul>
-    </>
+    <ul>
+      {persons.map((person) => (
+        <li key={person.name}>
+          <Profile {...person}/>
+        </li>
+      ))}
+    </ul>
   );
 };
 
