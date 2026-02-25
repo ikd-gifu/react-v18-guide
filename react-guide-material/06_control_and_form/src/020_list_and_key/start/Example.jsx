59ba6a23406d6432664a1c7b079312db4a19e390
ikd-gifu <oki19ki@gmail.com>
2026-02-25 09:43:35 +0900

    ss6 020 024  62 key

--- react-guide-material/06_control_and_form/src/020_list_and_key/start/Example.jsx
+++ react-guide-material/06_control_and_form/src/020_list_and_key/start/Example.jsx
@@ -19,12 +19,20 @@ const Example = () => {
         <li>{animals[2]}</li> */}
         {/* {animalList}
         {helloAnimals} */}
-        {/* {animalList} */}
-
-        {animals.map((animal) => <li>Hello, {animal}</li>)}
+        {animalList}
+        {/* 配列のような繰り返し処理を行う小要素にはkeyプロパティを必ずつける */}
+        {animals.map((animal) => <li key={animal}>Hello, {animal}</li>)}
       </ul>
     </>
   );
 };
 
 export default Example;
+
+// keyをつける際の注意点
+// ・keyには必ず一意の値を設定する
+// ・keyに設定した値は変更しない
+// ・配列のインデックスはなるべく使わない
+// →配列の先頭に追加すると、インデックスもズレて挙動がおかしくなる
+// 先頭に要素を新たに追加する場合、keyがないと他の子要素も全て更新（commit）する挙動になる
+// Reactはkeyを元に比較して差分を更新するため
