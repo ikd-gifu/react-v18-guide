d3de58fb2bbc9bf351d0416fffb3d5c4d8232ee7
ikd-gifu <oki19ki@gmail.com>
2026-02-27 10:07:56 +0900

    ss6 060 66 if && ??

--- react-guide-material/06_control_and_form/src/060_conditional_render/start/Example.jsx
+++ react-guide-material/06_control_and_form/src/060_conditional_render/start/Example.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 
 const Example = () => {
-  const animals = ["Dog", "Cat", "Rat"];
+  const animals = ["Dog", "Cat", null, "Rat"];
 
   const [filterVal, setFilterVal] = useState("");
 
+  /* POINT 条件分岐 if文、&&、??（Null合体演算子）、３項演算子
+  A && B (Aがfalsyの時Aを返し、Aがtruthyの時Bを返す)
+  A ?? B (Aがnull or undefinedの時、Bを使う)
+  */
   return (
     <>
       <input
@@ -15,16 +19,42 @@ const Example = () => {
       <ul>
         {animals
           .filter((animal) => {
-            const isMatch = animal.indexOf(filterVal) !== -1;
-            console.log(animal.indexOf(filterVal));
+            const animalString = animal ?? "";
+            const isMatch = animalString.indexOf(filterVal) !== -1;
+            console.log(animalString.indexOf(filterVal));
             return isMatch;
           })
-          .map((animal) => (
-            <li key={animal}>{animal}</li>
-          ))}
+          .map((animal) => {
+            // true のとき→ true && "⭐️" なので結果は "⭐️"
+            // false のとき→ false && "⭐️" なので結果は false
+            // （Reactは false を表示しない）
+            return <li key={animal}>
+              {animal ?? "null,undefinedでした"}{animal === "Dog" && "⭐️"}
+              </li>
+            // return <li key={animal}>{ // 三項演算子
+            //   animal + (
+            //   animal === "Dog"
+            //     ? "⭐️"
+            //     : "")
+            //   }</li>
+            // return <li key={animal}>{
+            //   animal === "Dog"
+            //     ? animal + "⭐️"
+            //     : animal
+            //   }</li>
+            // if (animal === "Dog") { // 重複コードが多い。三項演算子が適切
+            //   return <li key={animal}>{animal}⭐️</li>
+            // } else {
+            //   return <li key={animal}>{animal}</li>
+            // }
+          })}
       </ul>
     </>
   );
 };
 
 export default Example;
+
+// mapの記法
+// .map((animal) => ({})) // オブジェクトリテラルの{}
+// .map((animal) => {}) // アロー関数の記法
