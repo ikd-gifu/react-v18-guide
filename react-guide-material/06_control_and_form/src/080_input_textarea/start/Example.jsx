efeb8e93966f8e896706a1bfe7d7b307fe9d63e6
ikd-gifu <oki19ki@gmail.com>
2026-02-28 10:48:34 +0900

    ss6 080 68 input, textarea

--- react-guide-material/06_control_and_form/src/080_input_textarea/start/Example.jsx
+++ react-guide-material/06_control_and_form/src/080_input_textarea/start/Example.jsx
@@ -1,10 +1,34 @@
+import { useState } from 'react';
+
 const Example = () => {
+  const [val, setVal] = useState("");
+  // const clearValue = setVal(""); // その場で実行 無限ループ
+  const clearValue = () => setVal(""); // 関数を定義 
+
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <div>
+      {/* htmlForをinput, textareaのidと同じにすると関連付けできる */}
+      <label htmlFor='789'>ラベル</label>
+      <div>
+        {/* valueの値にinputの内容を紐付ける */}
+        {/* htmlではfor JSXではhtmlFor */}
+        <input
+          id="789"
+          placeholder='こんにちは'
+          value={val}
+          onChange={(e) => setVal(e.target.value)}
+        />
+        {/* htmlでは<></>の中に値を書くが、JSXは< />の中に書く */}
+        <textarea
+          id="1011"
+          placeholder='こんにちは'
+          value={val}
+          onChange={(e) => setVal(e.target.value)}
+        />
+      </div>
+      <h3>{val}</h3>
+      <button onClick={clearValue} >クリア</button>
+    </div>
   );
 };
 
